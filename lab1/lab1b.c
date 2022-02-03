931a8ae089e8ec27144d27a81caedc3ccb32a23a
Martin <martinmcchua@gmail.com>
2022-02-03 13:31:20 -0800

    Ok actually done lab 1 now

--- lab1/lab1b.c
+++ lab1/lab1b.c
@@ -1,21 +1,27 @@
+
 void MemoryTest(void)
 {
-    unsigned long long int *RamPtr, counter1=1 ;
+    unsigned long long int *RamPtr;
+    unsigned int counter1=0x8cb;
     register unsigned int i ;
     unsigned int Start, End ;
     char c ;
     unsigned long int data, write_data;
     unsigned int bwl=0;
 
-    // No input validation of addresses, user should know that for word and long word addresses shoudl align to even.
-    printf("\r\nStart Address (min 0x08020000 max 0x08030000): ") ;
-    // Start = Get8HexDigits(0) ;
-    Start = 0x08020000;
-    printf("\r\nEnd Address (min 0x08020000 max 0x08030000): ") ;
-    // End = Get8HexDigits(0) ;
-    End = 0x08030000;
+    // input validation of addresses, user should know that for word and long word addresses shoudl align to even.
+    do{
+        printf("\r\nStart Address (min 0x08020000 max 0x08030000): ") ;
+        Start = Get8HexDigits(0) ;
+    } while (0x08020000 > Start || 0x08030000 < Start);
+    // Start = 0x08020000;
+    do {
+        printf("\r\nEnd Address (min 0x08020000 max 0x08030000): ") ;
+        End = Get8HexDigits(0) ;
+    } while (Start > End || End > 0x08030000);
+    // End = 0x08030000;
 
-	// Get test pattern
+	// Get test pattern and set data appropriately
     while(1)    {
         FlushKeyboard() ;               // dump unread characters from keyboard
         printf("\r\nChoose test pattern: \r\n0: 55\r\n1: AA\r\n2: FF\r\n3: 00");
@@ -40,9 +46,10 @@ void MemoryTest(void)
         }
     }
 
+    // Let user know what they selected
     printf("\r\nData selected: 0x%x", data);
 
-    // Get size of test pattern
+    // Get size of test pattern and set data size appropriately
     while(1)    {
         FlushKeyboard() ;               // dump unread characters from keyboard
         printf("\r\nEnter 'B', 'W', or 'L' for bytes, words, or long word: ");
@@ -65,11 +72,13 @@ void MemoryTest(void)
         }   
     }
 	
+    // Let user know what they selected
     printf("\r\nbwl selected:");
     if(bwl == (unsigned int)(0x3)) printf("Long word");
     if(bwl == (unsigned int)(0x2)) printf("Word");
     if(bwl == (unsigned int)(0x1)) printf("Bytes");
 
+    // Set pointer to start addr
     RamPtr = Start;
     // Write loop
     while(1){
@@ -78,32 +87,41 @@ void MemoryTest(void)
             printf("\r\nWrite complete. Starting read.");
             break;
         }
+        // Set data at address to specified by user
         *RamPtr = data;
         
         counter1++;
-        if (counter1 == 31){
+        // Dont check every time, just check some time incl first time
+        if (counter1 == 0x8cc){
             printf("\r\nWrite: 0x%x to addr 0x%x", *RamPtr, RamPtr);
             counter1 = 1;
         }
+        // Increment address
         RamPtr++;
     }
     
+    // Reset pointer to start addr
     RamPtr = Start;
-    counter1 = 1;
+    // Reset counter to default
+    counter1 = 0x8cb;
 
     // Read loop
     while(1){
+        // When end addr is reached
         if (RamPtr > End){
+            printf("\r\nRead complete.");
             printf("\r\nPASS: Mem test completed with no errors.");
             break;
         }
+        // Read check every address to specified data by user
         if (*RamPtr != data){
             printf("\r\nERROR: Address 0x%x data is 0x%x but should be 0x%x", RamPtr, *RamPtr, data);
-            printf("\r\nStart is 0x%x", Start);
+            printf("\r\nFAIL: Mem test did not complete successfully.");
             break;
         }
         counter1++;
-        if (counter1 == 31){
+        // Dont check every time, just check some time incl first time
+        if (counter1 == 0x8cc){
             printf("\r\nRead: Address 0x%x data is 0x%x", RamPtr, *RamPtr);
             counter1 = 1;
         }
